e4db15ab07caa24e8c4226dd4a91e40ce9024d95
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2021-06-17 15:29:27 -0300

    add fase green tdd

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const routes = Router();
 const SessionController = require('./app/controllers/SessionController')
 
 routes.post('/sessions', SessionController.store)
+routes.get('/dashboard', (req, res) => {
+  return res.status(200).send()
+})
 
 module.exports = routes;
